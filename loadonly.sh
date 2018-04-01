ee9e1dc6a3d4b8e4b90cd4770d66f4c6d7f9479a
Qian Li <liqian.cs@gmail.com>
2018-04-01 16:39:23 -0600

    Add loadonly.sh script to load data for experiments

---
+++ loadonly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script is used to load data into memcached server
+
+scriptname=`basename "$0"`
+if [[ "$#" -ne 4 ]]; then
+    echo "Usage: $scriptname <server> <key-min> <key-max> <data-size>"
+    echo "Example: $scriptname n1 100000 200000 32"
+    exit
+fi
+
+server=$1
+keymin=$2
+keymax=$3
+datasize=$4
+let "reqs = ($keymax - $keymin) / 16 + 1"
+
+cmd="./memtier_benchmark -s $server -p 11211 -P memcache_binary --clients=1 \
+    --threads 16 --ratio 1:0 --run-count=1 --pipeline=100 \
+    --key-minimum=$keymin --key-maximum=$keymax \
+    --data-size=$datasize --random-data --hide-histogram \
+    --key-pattern=P:P --requests=$reqs"
+
+# execute the commnad
+echo $cmd
+$cmd
