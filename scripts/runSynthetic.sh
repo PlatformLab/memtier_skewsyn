659e91056393d436851375d8f1239dd30e263fba
Qian Li <liqian.cs@gmail.com>
2018-04-20 14:34:11 -0600

    Can use multiple clients to generate load and one master to collect latencies
    And control key prefix to pad to 30-byte keys

--- scripts/runSynthetic.sh
+++ scripts/runSynthetic.sh
@@ -3,10 +3,11 @@
 # This script is used to run synthetic workload
 
 scriptname=`basename "$0"`
-if [[ "$#" -ne 8 ]]; then
-    echo "Usage: $scriptname <server> <key-min> <key-max> <data-size> " \
-         "<iterations> <synthetic_bench> <num-videos> <prefix: arachne/origin>"
-    echo "Example: $scriptname n1 100000 200000 32 5 workloads/LoadEstimator.bench 1 arachne"
+if [[ "$#" -lt 8 ]]; then
+    echo "Usage: $scriptname <server> <key-min> <key-max> <data-size>" \
+         "<iterations> <synthetic_bench> <num-videos> <prefix: arachne/origin>" \
+         "[list of clients...]"
+    echo "Example: $scriptname n1 100000 200000 32 5 workloads/LoadEstimator.bench 1 arachne n3 n4..."
     exit
 fi
 
@@ -25,10 +26,12 @@ videos=$7
 prefix=$8
 
 clients=16
-threads=16
+threads=32
 ratio="0:1"
-pipeline=10
+pipeline=4
 irdist="POISSON"
+keyprefix="memtierxxxxxxxxxxxxxxx-" # Pad key to achieve 30-byte
+keypattern="G:G"
 
 logdir=exp_logs/${prefix}_iters${iters}_synthetic_logs
 qpsprefix="qps"
@@ -40,21 +43,67 @@ rm -rf $logdir/* # Clear previous logs
 rm $runlog
 
 # Load data into Memcached and warm up
-cmd="bash $originalMemtier/loadonly.sh $server $keymin $keymax $datasize"
+cmd="bash $originalMemtier/loadonly.sh $server $keymin $keymax $datasize $keyprefix"
 echo $cmd
 $cmd >> $runlog 2>&1
 
 # Execute experiments multiple times
 for iter in `seq 1 $iters`;
 do
+    # $9... will be client addresses, start clients!
+    if [[ "$#" -gt 8 ]]; then
+        # shift previous ones
+        for argn in `seq 1 8`;
+        do
+            shift
+        done
+
+        clientlogdir=$HOME/client_logs/$logdir
+        clientbench=$HOME/memtier_benchmark_skewsyn/$benchfile
+        mkdir -p $clientlogdir
+        rm -rf $clientlogdir/*
+
+        for client in "$@";
+        do
+            echo "Starting client: $client ..."
+            clientqpsfile=${qpsprefix}_iter${iter}_${client}.csv
+            clientRunLog=$HOME/client_logs/${runlog}_${client}
+            cmd="$HOME/memtier_benchmark_skewsyn/memtier_benchmark -s $server \
+                 -p 11211 -P memcache_binary --clients $clients --threads $threads \
+                 --ratio $ratio --pipeline=$pipeline \
+                 --key-prefix=$keyprefix \
+                 --key-minimum=$keymin --key-maximum=$keymax \
+                 --data-size=$datasize --random-data --hide-histogram \
+                 --key-pattern=$keypattern --run-count=1 \
+                 --distinct-client-seed --randomize \
+                 --test-time=1 -b \
+                 --config-file=$clientbench --ir-dist=$irdist \
+                 --log-dir=${clientlogdir} \
+                 --log-qpsfile=$clientqpsfile"
+            #echo $cmd
+            sshcmd="ssh -p 5515 $client \"nohup $cmd > $clientRunLog 2>&1 < /dev/null &\""
+            echo $sshcmd
+            ssh -p 5515 $client "nohup $cmd > $clientRunLog 2>&1 < /dev/null & "
+        done
+        # Change parameters for the master
+        clients=1
+        threads=8
+        pipeline=1
+        irdist="UNIFORM"
+        benchfile=workloads/Synthetic16-master.bench
+    fi
+
+    # Master usually run in non-blocking mode
     logqpsfile=${qpsprefix}_iter${iter}.csv
     latencyfile=${latencyprefix}_iter${iter}.csv
     cmd="./memtier_benchmark -s $server -p 11211 -P memcache_binary \
         --clients $clients --threads $threads --ratio $ratio \
         --pipeline=$pipeline \
+        --key-prefix=$keyprefix \
         --key-minimum=$keymin --key-maximum=$keymax \
         --data-size=$datasize --random-data --hide-histogram \
-        --key-pattern=R:R --run-count=1 --distinct-client-seed --randomize \
+        --key-pattern=$keypattern --run-count=1 \
+        --distinct-client-seed --randomize \
         --test-time=1 -b \
         --config-file=$benchfile --ir-dist=$irdist --log-dir=$logdir \
         --log-qpsfile=$logqpsfile --log-latencyfile=$latencyfile \
@@ -80,10 +129,10 @@ mv $logdir/${qpsprefix}_iter* $qpsdir
 mv $logdir/${latencyprefix}_iter* $latencydir
 
 # Merge the data
-cmd="scripts/mergeStats.py ${qpsdir} ${prefix}_iters${iters}_qps"
-echo $cmd
-$cmd >> $runlog 2>&1
-
-cmd="scripts/mergeStats.py ${latencydir} ${prefix}_iters${iters}_latency"
-echo $cmd
-$cmd >> $runlog 2>&1
+#cmd="scripts/mergeStats.py ${qpsdir} ${prefix}_iters${iters}_qps"
+#echo $cmd
+#$cmd >> $runlog 2>&1
+#
+#cmd="scripts/mergeStats.py ${latencydir} ${prefix}_iters${iters}_latency"
+#echo $cmd
+#$cmd >> $runlog 2>&1
