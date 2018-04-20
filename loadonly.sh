f3489c0b88d623bbc7ec60b31e22db352589d32f
Qian Li <liqian.cs@gmail.com>
2018-04-20 14:33:11 -0600

    Add key-prefix option in loadonly.sh

--- loadonly.sh
+++ loadonly.sh
@@ -3,9 +3,9 @@
 # This script is used to load data into memcached server
 
 scriptname=`basename "$0"`
-if [[ "$#" -ne 4 ]]; then
-    echo "Usage: $scriptname <server> <key-min> <key-max> <data-size>"
-    echo "Example: $scriptname n1 100000 200000 32"
+if [[ "$#" -lt 4 ]]; then
+    echo "Usage: $scriptname <server> <key-min> <key-max> <data-size> (<key-prefix>)"
+    echo "Example: $scriptname n1 100000 200000 32 memtier-"
     exit
 fi
 
@@ -17,13 +17,18 @@ server=$1
 keymin=$2
 keymax=$3
 datasize=$4
+keyprefix="memtier-"
+if [[ "$#" -ge 5 ]]; then
+keyprefix=$5
+fi
+
 let "reqs = ($keymax - $keymin) / 16 + 1"
 
 cmd="./memtier_benchmark -s $server -p 11211 -P memcache_binary --clients=1 \
     --threads 16 --ratio 1:0 --run-count=1 --pipeline=100 \
     --key-minimum=$keymin --key-maximum=$keymax \
     --data-size=$datasize --random-data --hide-histogram \
-    --key-pattern=P:P --requests=$reqs"
+    --key-pattern=P:P --requests=$reqs --key-prefix=$keyprefix"
 
 # execute the commnad
 echo $cmd
@@ -33,7 +38,7 @@ $cmd
 let "reqs = $reqs * 2"
 cmd="./memtier_benchmark -s $server -p 11211 -P memcache_binary --clients=1 \
     --threads 16 --ratio 0:1 --run-count=1 --pipeline=100 \
-    --key-minimum=$keymin --key-maximum=$keymax \
+    --key-minimum=$keymin --key-maximum=$keymax --key-prefix=$keyprefix \
     --data-size=$datasize --random-data --hide-histogram \
     --key-pattern=P:P --requests=$reqs"
 
