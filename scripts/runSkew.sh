d6d748722c0a21a0cc4282dc22bf933336f56950
Qian Li <liqian.cs@gmail.com>
2018-04-23 23:38:38 -0600

    Update skewed benchmark to use gausian distribution for key access
    Currently runSkew.sh cannot support multiple client machines.
    But I implemented the support for client options for future use.

--- scripts/runSkew.sh
+++ scripts/runSkew.sh
@@ -3,10 +3,11 @@
 # This script is used to run skewed workload
 
 scriptname=`basename "$0"`
-if [[ "$#" -ne 7 ]]; then
+if [[ "$#" -lt 7 ]]; then
     echo "Usage: $scriptname <server> <key-min> <key-max> <data-size> " \
-         "<iterations> <skew_bench> <prefix: arachne/origin>"
-    echo "Example: $scriptname n1 100000 200000 32 5 workloads/Skew.bench arachne"
+         "<iterations> <skew_bench> <prefix: arachne/origin>" \
+         "[list of clients...]"
+    echo "Example: $scriptname n1 100000 200000 32 5 workloads/Skew.bench arachne n4..."
     exit
 fi
 
@@ -29,6 +30,7 @@ ratio="0:1"
 pipeline=100
 irdist="POISSON"
 keyprefix="memtierxxxxxxxxxxxxxxx-" # Pad key to achieve 30-byte
+keypattern="G:G"
 
 videos=0 # No background videos
 
@@ -49,6 +51,41 @@ $cmd >> $runlog 2>&1
 # Execute experiments multiple times
 for iter in `seq 1 $iters`;
 do
+    # $9... will be client addresses, start clients!
+    if [[ "$#" -gt 7 ]]; then
+        # shift previous ones
+        for argn in `seq 1 7`;
+        do
+            shift
+        done
+
+        clientlogdir=$HOME/memtier_benchmark_skewsyn/$logdir
+        clientbench=$HOME/memtier_benchmark_skewsyn/$benchfile
+
+        for client in "$@";
+        do
+            echo "Starting client: $client ..."
+            clientqpsfile=${qpsprefix}_iter${iter}_${client}.csv
+            clientRunLog=$HOME/memtier_benchmark_skewsyn/${runlog}_${client}
+            cmd="$HOME/memtier_benchmark_skewsyn/memtier_benchmark -s $server \
+                 -p 11211 -P memcache_binary --clients $clients --threads $threads \
+                 --ratio $ratio --pipeline=$pipeline \
+                 --key-prefix=$keyprefix \
+                 --key-minimum=$keymin --key-maximum=$keymax \
+                 --data-size=$datasize --random-data --hide-histogram \
+                 --key-pattern=$keypattern --run-count=1 \
+                 --distinct-client-seed --randomize \
+                 --test-time=1 -b \
+                 --config-file=$clientbench --ir-dist=$irdist \
+                 --log-dir=${clientlogdir} \
+                 --log-qpsfile=$clientqpsfile"
+            #echo $cmd
+            sshcmd="ssh -p 5515 $client \"nohup $cmd > $clientRunLog 2>&1 < /dev/null &\""
+            echo $sshcmd
+            ssh -p 5515 $client "nohup $cmd > $clientRunLog 2>&1 < /dev/null & "
+        done
+    fi
+
     logqpsfile=${qpsprefix}_iter${iter}.csv
     #latencyfile=${latencyprefix}_iter${iter}.csv
     cmd="./memtier_benchmark -s $server -p 11211 -P memcache_binary \
@@ -57,7 +94,7 @@ do
         --key-minimum=$keymin --key-maximum=$keymax \
         --data-size=$datasize --random-data --hide-histogram \
         --key-prefix=$keyprefix \
-        --key-pattern=R:R --run-count=1 --distinct-client-seed --randomize \
+        --key-pattern=$keypattern --run-count=1 --distinct-client-seed --randomize \
         --test-time=1 -b \
         --config-file=$benchfile --ir-dist=$irdist --log-dir=$logdir \
         --log-qpsfile=$logqpsfile \
